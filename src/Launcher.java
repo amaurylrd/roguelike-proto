fefc4bc243c3a08471ee36be463638a803ed34b4
amaurylrd <amaurylrd@yahoo.fr>
2020-10-17 19:30:52 +0200

    FocusableWindow javadoc

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,3 @@
-import java.util.Locale;
-
 import engine.application.Application;
 import engine.stage.Stage;
 import engine.stage.Stage.Style;
@@ -15,7 +13,6 @@ public class Launcher extends Application {
         primaryStage.setTitle("salut");
         primaryStage.setSize(400, 400);
         primaryStage.centerOnScreen();
-        boolean isCompatible =  !System.getProperty("os.name").toLowerCase().contains("win");
         
         //Routine loop = new Routine(primaryStage);
         //loop.start();
