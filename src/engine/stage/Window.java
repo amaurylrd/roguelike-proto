d129f7c6bf0d542b8e0e766bc91df848a2d6a9ae
amaurylrd <amaurylrd@yahoo.fr>
2020-11-04 19:26:17 +0100

    Window focus

--- src/engine/stage/Window.java
+++ src/engine/stage/Window.java
@@ -1,16 +1,19 @@
 package engine.stage;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.awt.Rectangle;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public abstract class Window extends JFrame {
+public abstract class Window extends JFrame implements WindowFocusListener {
     public Window() {
         super();
         setBounds(Screen.getBounds());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setFocusable(true);
-		setFocusTraversalKeysEnabled(false);
+        setFocusTraversalKeysEnabled(false);
+        addWindowFocusListener(this);
     }
 
     /**
@@ -24,4 +27,24 @@ public abstract class Window extends JFrame {
         double centerY = bounds.getMinY() + (clientHeight - windowHeight) / 2;
         setLocation(Double.isNaN(centerX) ? 0 : (int) centerX, Double.isNaN(centerY) ? 0 : (int) centerY);
     }
+
+    /**
+     * Invoked when the window is set to be the focused Window, which means
+     * that the Window, or one of its subcomponents, will receive keyboard
+     * events.
+     * 
+     * @param event the event to be processed
+     */
+    @Override
+    public void windowGainedFocus(WindowEvent event) {}
+
+    /**
+     * Invoked when the window is no longer the focused Window, which means
+     * that keyboard events will no longer be delivered to the Window or any of
+     * its subcomponents.
+     * 
+     * @param event the event to be processed
+     */
+    @Override
+    public void windowLostFocus(WindowEvent event) {}
 }
