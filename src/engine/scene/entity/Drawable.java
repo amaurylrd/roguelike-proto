5a514ca8f566bd334797ea5ffbabd5632e6a3fb8
amaurylrd <amaurylrd@yahoo.fr>
2020-12-29 12:00:56 +0100

    Drawable comments

--- src/engine/scene/entity/Drawable.java
+++ src/engine/scene/entity/Drawable.java
@@ -2,8 +2,21 @@ package engine.scene.entity;
 
 import java.awt.Graphics2D;
 
+/**
+ * This interface {@code Drawable} defines any graphical game object.
+ */
 public interface Drawable {
+    /**
+     * Called whenever a game object needs to be updated.
+     * 
+     * @param dt the delta time between updates
+     */
     public abstract void update(float dt);
 
+    /**
+     * Called to render a game object into the scene.
+     * 
+     * @param graphics the canvas graphics context
+     */
     public abstract void render(Graphics2D graphics);
 }
