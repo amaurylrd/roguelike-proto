98b5ce76cf00a75f1ffa5cac967f193013bd1800
amaurylrd <amaurylrd@yahoo.fr>
2020-12-31 05:41:33 +0100

    Camera lerp

--- src/engine/scene/Camera.java
+++ src/engine/scene/Camera.java
@@ -16,11 +16,11 @@ public class Camera {
 
     protected void update(float dt) {
         if (scene.player != null) {
-            Rectangle bounds = scene.player.getBounds();
-            double xoff = bounds.getX() + bounds.getWidth()/2 - scene.getWidth()/2.2;
-            double yoff = bounds.getY() + bounds.getHeight()/2 - scene.getHeight()/2.2;
-            position.translateX((xoff - position.getX()) * 0.1);
-            position.translateY((yoff - position.getY()) * 0.02);
+            Rectangle playerBox = scene.player.getBounds();
+            double targetX = playerBox.getX() + playerBox.getWidth()/2 - scene.getWidth()/2.2;
+            double targetY = playerBox.getY() + playerBox.getHeight()/2 - scene.getHeight()/2.2;
+            position.translateX((targetX - position.getX()) * 0.1);
+            position.translateY((targetY - position.getY()) * 0.02);
             //offset.translate(xoff, yoff);
             //offset.set(lerp(offset.getX(), xoff, dt), lerp(offset.getY(), yoff, dt));
         }
@@ -33,14 +33,6 @@ public class Camera {
     protected double getY() {
         return position.getY();
     }
-
-    // protected void update(float dt) {
-    //     if (target != null) {
-    //         float xoff = lerp(offset.getX(), target.getBounds().getX(), dt);
-    //         float yoff = lerp(offset.getY(), target.getBounds().getY(), dt);
-    //         offset = new Vector(xoff, yoff);
-    //     }
-    // }
     
     public static double lerp(double a, double b, double f) {
 		return a + f*(b - a);
