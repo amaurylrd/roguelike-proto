468e017e6f338ce9a575c7658a962683dbd85c10
amaurylrd <amaurylrd@yahoo.fr>
2021-04-05 03:01:21 +0200

    opengl vertex shader

--- ressources/shaders/vertex_shader.glsl
+++ ressources/shaders/vertex_shader.glsl
@@ -1,12 +1,12 @@
 #version 150
 
-in vec3 in_position;
+in vec2 in_position;
 
 out vec3 out_color;
 
 void 	main(void) {
 
-	gl_Position = vec4(in_position, 1.0);
+	gl_Position = vec4(in_position, 0.0, 1.0);
 
 	//colour = vec3(position.x + 0.5, 0.0, position.y + 0.5);
 	out_color = vec3(255.0, 0.0, 0.0);
